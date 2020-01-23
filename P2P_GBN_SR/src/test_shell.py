e6c7c77741285afdc307cceb204c026bc8745994
Demian Lesik <lesikdjem@yandex.ru>
2020-01-23 18:05:20 +0300

    Added results

--- P2P_GBN_SR/src/test_shell.py
+++ P2P_GBN_SR/src/test_shell.py
@@ -12,7 +12,7 @@ def stat_plot(x_list, y_list_gbn, y_list_sr, x_label, y_label, file_name='../res
     plt.close()
 
 
-def pack_number(
+def arq_test_shell(
         arg_list: list,
         tries: int,
         arq_mode: str,  # 'gbn' or 'sr'
@@ -49,6 +49,7 @@ def pack_number(
                 exp = expr.Experiment(arq_mode, ws, prob, transfer_number=transfer_packs)
                 result += exp.calc_time()
 
+        print('done', arg)
         result = int(result / tries)
         output_list.append(result)
 
@@ -57,22 +58,42 @@ def pack_number(
 
 def test_pack_lose():
     lose_pr = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]  # lose probability list
-    sr_pack = pack_number(lose_pr, 4, 'sr', test_mode='number', window_size=4, session_seconds=20)
-    gbn_pack = pack_number(lose_pr, 4, 'gbn', test_mode='number', window_size=4, session_seconds=20)
+    sr_pack = arq_test_shell(lose_pr, 4, 'sr', test_mode='number', window_size=4, session_seconds=20)
+    gbn_pack = arq_test_shell(lose_pr, 4, 'gbn', test_mode='number', window_size=4, session_seconds=20)
 
     stat_plot(lose_pr, gbn_pack, sr_pack,
               'Package lose probability', 'Packages transferred', '../results/pack_lose.png')
 
 
 def test_pack_window():
-    window_size = [1, 3, 5]  # [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]  # lose probability list
-    sr_pack = pack_number(window_size, 1, 'sr', test_mode='number', probability=0.3, session_seconds=4)
-    gbn_pack = pack_number(window_size,  1, 'gbn', test_mode='number', probability=0.3, session_seconds=4)
+    window_size = [1, 2, 3, 4, 6, 8, 10, 12]
+    sr_pack = arq_test_shell(window_size, 4, 'sr', test_mode='number', probability=0.15, session_seconds=20)
+    gbn_pack = arq_test_shell(window_size,  4, 'gbn', test_mode='number', probability=0.15, session_seconds=20)
 
     stat_plot(window_size, gbn_pack, sr_pack,
               'Package window size', 'Packages transferred', '../results/pack_window.png')
 
 
+def test_time_lose():
+    lose_pr = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]  # lose probability list
+    sr_pack = arq_test_shell(lose_pr, 4, 'sr', test_mode='time', window_size=4, transfer_packs=50)
+    gbn_pack = arq_test_shell(lose_pr, 4, 'gbn', test_mode='time', window_size=4, transfer_packs=50)
+
+    stat_plot(lose_pr, gbn_pack, sr_pack,
+              'Package lose probability', 'Second for transmission', '../results/time_lose.png')
+
+
+def test_time_window():
+    window_size = [1, 2, 3, 4, 6, 8, 10, 12]
+    sr_pack = arq_test_shell(window_size, 4, 'sr', test_mode='time', probability=0.15, transfer_packs=500)
+    gbn_pack = arq_test_shell(window_size,  4, 'gbn', test_mode='time', probability=0.15, transfer_packs=500)
+
+    stat_plot(window_size, gbn_pack, sr_pack,
+              'Package window size', 'Second for transmission', '../results/time_window.png')
+
+
 if __name__ == '__main__':
-    # test_pack_lose()
+    test_pack_lose()
     test_pack_window()
+    test_time_lose()
+    test_time_window()
