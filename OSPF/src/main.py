99dfc1d19aa483d40c7d99e148911a48bff810c9
Demian Lesik <lesikdjem@yandex.ru>
2020-02-29 23:36:52 +0300

    Minor process call order changed

--- OSPF/src/main.py
+++ OSPF/src/main.py
@@ -9,18 +9,12 @@ WHITE = (255, 255, 255)
 ws = 480  # window size
 
 
-class Point:
-    def __init__(self, x: float, y: float):
-        self.x = x
-        self.y = y
-
-
 def display(display_queue: mp.Queue):
     import pygame
     pygame.init()
     sc = pygame.display.set_mode((ws, ws))
     sc.fill(WHITE)
-
+    radius_size = 20
     font = pygame.font.Font(None, 36)
 
     while True:
@@ -30,56 +24,39 @@ def display(display_queue: mp.Queue):
 
         try:
             routers_meta_list: list = display_queue.get(timeout=0.25)
+            print(len(routers_meta_list))
             for meta in routers_meta_list:
-                pygame.draw.circle(sc, ORANGE, (int(meta.x * ws), int(meta.y * ws)), 20)
+                pygame.draw.circle(sc, ORANGE, (int(meta.x * ws), int(meta.y * ws)), radius_size)
                 text = font.render(str(meta.id), 1, (180, 0, 0))
-                sc.blit(text, (int(meta.x * ws), int(meta.y * ws)))
+                sc.blit(text, (int(meta.x * (ws -  radius_size // 2)), int(meta.y * (ws -  radius_size // 2))))
         except Empty:
             pass
 
         pygame.display.update()
 
 
-def run(main_process_queue: mp.Queue):
+if __name__ == "__main__":
     net = Net()
     display_queue = mp.Queue()  # for update display
     display_process = mp.Process(target=display, args=(display_queue,))
     display_process.start()
-
+    time.sleep(2)
     while True:
-        action: Action = main_process_queue.get()
+        action: Action = parser.cmd_parse()
         if action.actionType == ActionType.Exit:
             break
 
+        print('aaaaa')
         action.start(net)
+        print('bbbbb')
         routers_meta_list = []
         for router in net.routers.values():
             routers_meta_list.append(router.meta)
+
+        print(action.actionType, routers_meta_list)
 
         display_queue.put(routers_meta_list)
 
     display_process.terminate()
     net.terminate()
-
-
-if __name__ == "__main__":
-    main_process_queue = mp.Queue()
-    main_process = mp.Process(target=run, args=(main_process_queue,))
-    main_process.start()
-
-    time.sleep(2)
-
-    while True:
-        action = parser.cmd_parse()
-
-        if action.actionType != ActionType.None_:
-            main_process_queue.put(action)
-
-        if action.actionType == ActionType.Exit:
-            break
-
-    main_process.join()
-
-
-
 
